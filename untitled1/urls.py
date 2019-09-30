c0471925807cbc321884d67e5ed0b6d2cf62c72f
Justin-Bee <jbee.appz@gmail.com>
2019-09-30 18:48:57 -0400

    added 2nd page

--- untitled1/urls.py
+++ untitled1/urls.py
@@ -21,5 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    path('page2', views.page2),
 ]
 
--- untitled1/views.py
+++ untitled1/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def page2(request):
+    return render(request, 'page2.html')
